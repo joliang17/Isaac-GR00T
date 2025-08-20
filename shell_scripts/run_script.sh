d114a577e71f1599e3794bf8736101fa16b45639
joliang17 <47235014+joliang17@users.noreply.github.com>
2025-08-20 12:58:10 -0400

    add lora training for eagle languague part

--- shell_scripts/run_script.sh
+++ shell_scripts/run_script.sh
@@ -6,4 +6,4 @@
 # python scripts/gr00t_finetune.py --dataset-path /fs/nexus-projects/wilddiffusion/vla/RoboMind/h5_simulation_lerobot --num-gpus 1
 
 
-python scripts/gr00t_finetune.py --dataset-path /fs/nexus-projects/wilddiffusion/vla/RoboMind/h5_simulation_lerobot --num-gpus 1 --lora_rank 32  --lora_alpha 128  --batch-size 24
+python scripts/gr00t_finetune.py --dataset-path /fs/nexus-projects/wilddiffusion/vla/RoboMind/h5_simulation_lerobot --num-gpus 1 --lora_rank 32  --lora_alpha 128  --batch-size 24 --lora_llm_model
