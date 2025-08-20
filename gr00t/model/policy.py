4341ce2b97dfdff57477f07ea5e865527583283a
joliang17 <47235014+joliang17@users.noreply.github.com>
2025-08-20 11:55:13 -0400

    load robomind dataset

--- gr00t/model/policy.py
+++ gr00t/model/policy.py
@@ -107,6 +107,7 @@ class Gr00tPolicy(BasePolicy):
         # Load model
         self._load_model(model_path)
         # Load transforms
+        import pdb;pdb.set_trace()
         self._load_metadata(self.model_path / "experiment_cfg")
         # Load horizons
         self._load_horizons()
@@ -241,7 +242,8 @@ class Gr00tPolicy(BasePolicy):
         metadata_path = exp_cfg_dir / "metadata.json"
         with open(metadata_path, "r") as f:
             metadatas = json.load(f)
-
+        
+        import pdb;pdb.set_trace()
         # Get metadata for the specific embodiment
         metadata_dict = metadatas.get(self.embodiment_tag.value)
         if metadata_dict is None:
