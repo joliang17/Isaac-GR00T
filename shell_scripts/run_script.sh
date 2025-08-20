84079428ac9f6c8e9dd6b04596e1633b642d8c20
joliang17 <47235014+joliang17@users.noreply.github.com>
2025-08-20 12:05:58 -0400

    add cmd

--- shell_scripts/run_script.sh
+++ shell_scripts/run_script.sh
@@ -3,4 +3,7 @@
 # finetune
 # python scripts/gr00t_finetune.py --dataset-path ./demo_data/robot_sim.PickNPlace --num-gpus 1
 
-python scripts/gr00t_finetune.py --dataset-path /fs/nexus-projects/wilddiffusion/vla/RoboMind/h5_simulation_lerobot --num-gpus 1
+# python scripts/gr00t_finetune.py --dataset-path /fs/nexus-projects/wilddiffusion/vla/RoboMind/h5_simulation_lerobot --num-gpus 1
+
+
+python scripts/gr00t_finetune.py --dataset-path /fs/nexus-projects/wilddiffusion/vla/RoboMind/h5_simulation_lerobot --num-gpus 1 --lora_rank 32  --lora_alpha 128  --batch-size 24
